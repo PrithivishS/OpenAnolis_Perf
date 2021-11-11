b1250d0c7cae5def3f8104d85b0e09440d5da110
Like Xu <like.xu@linux.intel.com>
2021-11-11 02:07:27 +0000

    perf/core: Rework guest callbacks to prepare for static_call support manual commit b9f5621c9547dd787900f005a9e1c3d5712de512 upstream To prepare for using static_calls to optimize perf's guest callbacks, replace ->is_in_guest and ->is_user_mode with a new multiplexed hook ->state, tweak ->handle_intel_pt_intr to play nice with being called when there is no active guest, and drop "guest" from ->get_guest_ip.
    Return '0' from ->state and ->handle_intel_pt_intr to indicate "not in
    guest" so that DEFINE_STATIC_CALL_RET0 can be used to define the static
    calls, i.e. no callback == !guest.
    
    [sean: extracted from static_call patch, fixed get_ip() bug, wrote changelog]
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Originally-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Like Xu <like.xu@linux.intel.com>
    Signed-off-by: Zhu Lingshan <lingshan.zhu@intel.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20211111020738.2512932-7-seanjc@google.com

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -9910,7 +9910,7 @@ static int perf_uprobe_event_init(struct perf_event *event)
 		return -EOPNOTSUPP;
 
 	is_retprobe = event->attr.config & PERF_PROBE_CONFIG_IS_RETPROBE;
-	ref_ctr_offset = event->attr.config >> PERF_UPROBE_REF_CTR_OFFSET_SHIFT;
+	ref_ctr_offset = event->attr.config  PERF_UPROBE_REF_CTR_OFFSET_SHIFT;
 	err = perf_uprobe_init(event, ref_ctr_offset, is_retprobe);
 	if (err)
 		return err;

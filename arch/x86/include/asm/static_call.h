3d7e248e5c0daee7449a4835022c09b872a3e6a3
Christophe Leroy <christophe.leroy@csgroup.eu>
2022-03-14 11:27:35 +0100

    static_call: Properly initialise DEFINE_STATIC_CALL_RET0()
    commit 5517d500829c683a358a8de04ecb2e28af629ae5 upstream
    
    When a static call is updated with __static_call_return0() as target,
    arch_static_call_transform() set it to use an optimised set of
    instructions which are meant to lay in the same cacheline.
    
    But when initialising a static call with DEFINE_STATIC_CALL_RET0(),
    we get a branch to the real __static_call_return0() function instead
    of getting the optimised setup:
    
    	c00d8120 <__SCT__perf_snapshot_branch_stack>:
    	c00d8120:	4b ff ff f4 	b       c00d8114 <__static_call_return0>
    	c00d8124:	3d 80 c0 0e 	lis     r12,-16370
    	c00d8128:	81 8c 81 3c 	lwz     r12,-32452(r12)
    	c00d812c:	7d 89 03 a6 	mtctr   r12
    	c00d8130:	4e 80 04 20 	bctr
    	c00d8134:	38 60 00 00 	li      r3,0
    	c00d8138:	4e 80 00 20 	blr
    	c00d813c:	00 00 00 00 	.long 0x0
    
    Add ARCH_DEFINE_STATIC_CALL_RET0_TRAMP() defined by each architecture
    to setup the optimised configuration, and rework
    DEFINE_STATIC_CALL_RET0() to call it:
    
    	c00d8120 <__SCT__perf_snapshot_branch_stack>:
    	c00d8120:	48 00 00 14 	b       c00d8134 <__SCT__perf_snapshot_branch_stack+0x14>
    	c00d8124:	3d 80 c0 0e 	lis     r12,-16370
    	c00d8128:	81 8c 81 3c 	lwz     r12,-32452(r12)
    	c00d812c:	7d 89 03 a6 	mtctr   r12
    	c00d8130:	4e 80 04 20 	bctr
    	c00d8134:	38 60 00 00 	li      r3,0
    	c00d8138:	4e 80 00 20 	blr
    	c00d813c:	00 00 00 00 	.long 0x0
    
    [Backport changes]
    
    1.In arch/powerpc/include/asm/static_call.h file, the `ifndef`
    block `_ASM_POWERPC_STATIC_CALL_H` was auto merged and it is
    required for current kernel.
    
    2.In include/linux/static_call.h file, the `DEFINE_STATIC_CALL_RET0`
    macro definition is already present in `static_call_init()` and
    `__static_call_return0()` function.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Link: https://lore.kernel.org/r/1e0a61a88f52a460f62a58ffc2a5f847d1f7d9d8.1647253456.git.christophe.leroy@csgroup.eu
    Signed-off-by: Jeevan Deep J <j.jeevandeep@amd.com>

--- arch/x86/include/asm/static_call.h
+++ arch/x86/include/asm/static_call.h
@@ -55,6 +55,8 @@
 #define ARCH_DEFINE_STATIC_CALL_RET0_TRAMP(name)			\
 	ARCH_DEFINE_STATIC_CALL_TRAMP(name, __static_call_return0)
 
+#define ARCH_DEFINE_STATIC_CALL_RET0_TRAMP(name)			\
+	ARCH_DEFINE_STATIC_CALL_TRAMP(name, __static_call_return0)
 
 #define ARCH_ADD_TRAMP_KEY(name)					\
 	asm(".pushsection .static_call_tramp_key, \"a\"		\n"	\
